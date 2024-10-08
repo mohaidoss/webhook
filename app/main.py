3528462acec9a7f10bdb9d3517a02c823d5b7eb3
mohaidoss <moussaif5@gmail.com>
2024-10-08 09:28:00 +0200

    disable swagger ui

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from api.webhook_controller import router as webhook_router
 
-app = FastAPI()
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+)
 
 
 app.include_router(webhook_router)
